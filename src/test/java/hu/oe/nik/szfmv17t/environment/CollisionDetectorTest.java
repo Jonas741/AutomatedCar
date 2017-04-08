4d2c7e273ab6aa96a59026f5df5017e7e82f4015
igyim <igyarto.imre@gmail.com>
2017-04-08 16:13:01 +0200

    Erich testje
    Erich testje

--- src/test/java/hu/oe/nik/szfmv17t/environment/CollisionDetectorTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/environment/CollisionDetectorTest.java
@@ -42,5 +42,128 @@ public class CollisionDetectorTest {
     public void differentObjectFarAway() throws Exception {
         assertEquals(CollisionDetector.collide(first, second), false);
     }
+    
+    @Test
+    public void Collide_Should_ReturnFalse_When_Called_With_NoObjects() throws Exception {
+        // Act.
+        boolean result = CollisionDetector.collide(null, null);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnFalse_When_Called_With_OneObject() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, null);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnTRue_When_Called_With_TheSameObject() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, firstObject);
+        
+        // Assert.
+        assertEquals(true, result);
+    }
 
+    @Test
+    public void Collide_Should_ReturnFalse_When_Called_With_TwoNonTouchingObjectsAtSamePositionXAndSameDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(100d, 112d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnTrue_When_Called_With_TwoNonTouchingObjectsAtSamePositionXAnd45DegreeDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(100d, 112d, 10d, 10d, 45d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnTrue_When_Called_With_TwoTouchingObjectsAtSamePositionXAndSameDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(100d, 95d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void Collide_Should_ReturnFalse_When_Called_With_TwoNonTouchingObjectsAtSamePositionYAndSameDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(112d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnTrue_When_Called_With_TwoNonTouchingObjectsAtSamePositionYAnd45DegreeDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(112d, 100d, 10d, 10d, 45d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void Collide_Should_ReturnTrue_When_Called_With_TwoTouchingObjectsAtSamePositionYAndSameDirection() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(95d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(true, result);
+    }
+    
+    @Test
+    public void Collide_Should_ReturnTrue_When_Called_With_TwoTouchingObjectsAtSamePosition() throws Exception {
+        // Arrange.
+        CollidableBase firstObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        CollidableBase secondObject = new Car(100d, 100d, 10d, 10d, 0d, 0, "test.jpg", 1000d, 10d, 0d);
+        
+        // Act.
+        boolean result = CollisionDetector.collide(firstObject, secondObject);
+        
+        // Assert.
+        assertEquals(true, result);
+    }
 }
